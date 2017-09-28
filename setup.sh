382d5646c2d35eeb8ed533af6ac9ab0bdc0a84da
FredJiang <270130108@qq.com>
2017-09-28 20:47:13 +0800

    no message

--- setup.sh
+++ setup.sh
@@ -1,219 +1,188 @@
 #! /bin/bash
 
 
-
-echo 'git pull'
-git pull
-
-
-
-echo 'git submodule init'
-git submodule init
+function echoCo {
+    echo -e "\033[32m$@\033[0m"
+}
 
 
-
-echo 'git submodule update'
-git submodule update
-
-
+function echoMe {
+    echo -e "\033[35m$@\033[0m"
+}
 
 
-
-echo 'cd ~/.tmux'
-cd ~/.tmux
-
+echoCo  git pull
+        git pull
 
 
+echoCo  git submodule init
+        git submodule init
 
 
+echoCo  git submodule update
+        git submodule update
+
+
+echoCo  cd ~/.tmux
+        cd ~/.tmux
 
 
 function install_tmux {
-    if which tmux > /dev/null
-    then
-        echo 'tmux has installed'
+    if which tmux > /dev/null; then
+        echoMe tmux has installed
     else
-        if which apt-get > /dev/null
-        then
-            echo 'sudo apt-get install -y tmux'
-                  sudo apt-get install -y tmux
+        if which apt-get > /dev/null; then
+            echoCo  sudo apt-get install -y tmux
+                    sudo apt-get install -y tmux
         fi
 
-        if which yum > /dev/null
-        then
-            echo 'sudo yum     install -y epel-release'
-                  sudo yum     install -y epel-release
-            echo 'sudo yum     install -y tmux'
-                  sudo yum     install -y tmux
+        if which yum > /dev/null; then
+            echoCo  sudo yum     install -y epel-release
+                    sudo yum     install -y epel-release
+            echoCo  sudo yum     install -y tmux
+                    sudo yum     install -y tmux
         fi
 
-        if which brew > /dev/null
-        then
-            echo 'brew         install   tmux'
-                  brew         install   tmux
+        if which brew > /dev/null; then
+            echoCo  brew         install    tmux
+                    brew         install    tmux
         fi
     fi
 }
+
 
 install_tmux
 
 
-
-
-
 function install_ruby {
-    if which ruby > /dev/null
-    then
-        echo 'ruby has installed'
+    if which ruby > /dev/null; then
+        echoMe 'ruby has installed'
     else
-        if which apt-get > /dev/null
-        then
-            echo 'sudo apt-get install -y ruby'
-                  sudo apt-get install -y ruby
+        if which apt-get > /dev/null; then
+            echoCo  sudo apt-get install -y ruby
+                    sudo apt-get install -y ruby
         fi
 
-        if which yum > /dev/null
-        then
-            echo 'sudo yum     install -y ruby'
-                  sudo yum     install -y ruby
+        if which yum > /dev/null; then
+            echoCo  sudo yum     install -y ruby
+                    sudo yum     install -y ruby
         fi
 
-        if which brew > /dev/null
-        then
-            echo 'brew         install    ruby'
-                  brew         install    ruby
+        if which brew > /dev/null; then
+            echoCo  brew         install    ruby
+                    brew         install    ruby
         fi
     fi
 }
+
 
 install_ruby
 
 
-
-
-
 function install_gem {
-    if which gem > /dev/null
-    then
-        echo 'gem has installed'
+    if which gem > /dev/null; then
+        echoMe 'gem has installed'
     else
-        if which apt-get > /dev/null
-        then
-            echo 'sudo apt-get install -y rubygems'
-                  sudo apt-get install -y rubygems
+        if which apt-get > /dev/null; then
+            echoCo  sudo apt-get install -y rubygems
+                    sudo apt-get install -y rubygems
         fi
 
-        if which yum > /dev/null
-        then
-            echo 'sudo yum     install -y rubygems'
-                  sudo yum     install -y rubygems
+        if which yum > /dev/null; then
+            echoCo  sudo yum     install -y rubygems
+                    sudo yum     install -y rubygems
         fi
 
-        if which brew > /dev/null
-        then
-            echo 'todo'
+        if which brew > /dev/null; then
+            echoMe 'todo'
         fi
     fi
 }
 
+
 install_gem
 
 
-
-
-
-
-if which tmuxinator > /dev/null
-then
-    echo 'tmuxinator has installed'
+if which tmuxinator > /dev/null; then
+    echoMe 'tmuxinator has installed'
 else
-    if [ $(which ruby | grep -c "$HOME") -eq 0 ];
-    then
-        echo 'sudo gem install tmuxinator'
-        sudo gem install tmuxinator
+    if [ $(which ruby | grep -c "$HOME") -eq 0 ]; then
+        echoCo  sudo gem install tmuxinator
+                sudo gem install tmuxinator
     else
-        echo 'gem install tmuxinator'
-        gem install tmuxinator
+        echoCo       gem install tmuxinator
+                     gem install tmuxinator
     fi
 fi
 
 
-echo 'cp .tmuxinator.zsh ~/.tmuxinator.zsh'
-cp .tmuxinator.zsh ~/.tmuxinator.zsh
+echoCo 'cp ~/.tmux/.tmuxinator.zsh ~/.tmuxinator.zsh'
+        cp ~/.tmux/.tmuxinator.zsh ~/.tmuxinator.zsh
 
 
-echo 'cp .tmux.conf ~/.tmux.conf'
-cp .tmux.conf ~/.tmux.conf
+echoCo 'cp ~/.tmux/.tmux.conf ~/.tmux.conf'
+        cp ~/.tmux/.tmux.conf ~/.tmux.conf
+
 
 tmuxVersion=$(tmux -V | cut -d" " -f2)
-echo tmuxVersion $tmuxVersion
-if [ "$(echo "$tmuxVersion <= 2.5" | bc -l)" -eq 1 ]
-then
-    grep -v plugin .tmux.conf > ~/.tmux.conf
+echoMe tmuxVersion $tmuxVersion
+if [ "$(echo "$tmuxVersion <= 2.5" | bc -l)" -eq 1 ]; then
+    echoCo  grep -v plugin .tmux.conf > ~/.tmux.conf
+            grep -v plugin .tmux.conf > ~/.tmux.conf
 fi
 
 
+echoCo  tmux source ~/.tmux.conf
+        tmux source ~/.tmux.conf
 
 
-
-echo 'tmux source ~/.tmux.conf'
-tmux source ~/.tmux.conf
-
-
-if [ -e ~/.zshrc ]
-then
-    grepResult=$(cat ~/.zshrc | grep '^export EDITOR="vim"')
-    echo ~/.zshrc $grepResult
-    if [ ${#grepResult} == 0 ]
-    then
-        echo 'export EDITOR="vim"'     >> ~/.zshrc
+if [ -e ~/.zshrc ]; then
+    echoCo       cat ~/.zshrc \| grep '^export EDITOR="vim"'
+    grepResult=$(cat ~/.zshrc  | grep '^export EDITOR="vim"')
+    if [ ${#grepResult} == 0 ]; then
+        echoCo  echo 'export EDITOR="vim"'     \>\> ~/.zshrc
+                echo 'export EDITOR="vim"'     >>   ~/.zshrc
     fi
 
-    grepResult=$(cat ~/.zshrc | grep '^source ~/.tmuxinator.zsh')
-    echo ~/.zshrc $grepResult
-    if [ ${#grepResult} == 0 ]
-    then
-        echo 'source ~/.tmuxinator.zsh' >> ~/.zshrc
+    echoCo       cat ~/.zshrc \| grep '^source ~/.tmuxinator.zsh'
+    grepResult=$(cat ~/.zshrc  | grep '^source ~/.tmuxinator.zsh')
+    if [ ${#grepResult} == 0 ]; then
+        echoCo  echo 'source ~/.tmuxinator.zsh' \>\> ~/.zshrc
+                echo 'source ~/.tmuxinator.zsh' >>   ~/.zshrc
     fi
 
-    echo $'cat ~/.zshrc | grep \'^export EDITOR="vim"\''
-    cat ~/.zshrc | grep '^export EDITOR="vim"'
+    echoCo  cat ~/.zshrc \| grep '^export EDITOR="vim"'
+            cat ~/.zshrc  | grep '^export EDITOR="vim"'
 
-    echo $'cat ~/.zshrc | grep \'^source ~/.tmuxinator.zsh\''
-    cat ~/.zshrc | grep '^source ~/.tmuxinator.zsh'
+    echoCo  cat ~/.zshrc \| grep '^source ~/.tmuxinator.zsh'
+            cat ~/.zshrc  | grep '^source ~/.tmuxinator.zsh'
 
-    /bin/zsh -c 'source ~/.zshrc'
+    echoCo  /bin/zsh -c 'source ~/.zshrc'
+            /bin/zsh -c 'source ~/.zshrc'
 fi
 
 
-if [ ! -e ~/.vimrc ]
-then
-    echo 'wget "https://raw.githubusercontent.com/FredJiang/.vim/master/.vimrc" -O ~/.vimrc'
-    wget "https://raw.githubusercontent.com/FredJiang/.vim/master/.vimrc" -O ~/.vimrc
+if [ ! -e ~/.vimrc ]; then
+    echoCo  wget "https://raw.githubusercontent.com/FredJiang/.vim/master/.vimrc" -O ~/.vimrc
+            wget "https://raw.githubusercontent.com/FredJiang/.vim/master/.vimrc" -O ~/.vimrc
 fi
 
 
-if [ -e ~/.tmux.conf ]
-then
+if [ -e ~/.tmux.conf ]; then
     grepResult=$(cat ~/.tmux.conf | grep '^source .*\/powerline\/bindings\/tmux\/powerline\.conf')
-    echo ~/.tmux.conf $grepResult
-    if [ ${#grepResult} == 0 ]
-    then
-        if which pip > /dev/null
-        then
+    echoCo       cat ~/.tmux.conf | grep '^source .*\/powerline\/bindings\/tmux\/powerline\.conf'
+    if [ ${#grepResult} == 0 ]; then
+        if which pip > /dev/null; then
             powerline_file=$(pip show powerline-status | awk '/Location:/{print $2 "/powerline/bindings/tmux/powerline.conf"}')
-            if [ -e $powerline_file ]
-            then
-                echo "source '$powerline_file'" >> ~/.tmux.conf
+            if [ -e $powerline_file ]; then
+                echoCo echo "source  '$powerline_file'"  \>\> ~/.tmux.conf
+                       echo "source  '$powerline_file'"  >>   ~/.tmux.conf
             fi
         fi
     fi
 
-    echo $'cat ~/.tmux.conf | grep $'^source .*\/powerline\/bindings\/tmux\/powerline\.conf''
-    cat ~/.tmux.conf | grep '^source .*\/powerline\/bindings\/tmux\/powerline\.conf'
+    echoCo cat ~/.tmux.conf | grep '^source .*\/powerline\/bindings\/tmux\/powerline\.conf'
+           cat ~/.tmux.conf | grep '^source .*\/powerline\/bindings\/tmux\/powerline\.conf'
 fi
-
-
-
 
 
